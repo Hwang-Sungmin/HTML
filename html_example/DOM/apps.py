7d6e5f7094239d4d0a8bfb8659ba59dd5c3ad35b
Hwang-Sungmin <hsm511@ajou.ac.kr>
2020-02-14 15:50:04 +0900

    reset

--- html_example/DOM/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DomConfig(AppConfig):
-    name = 'DOM'
--- html_example/RestApi/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Getdata(models.Model):
-    name = models.CharField(max_length=20)
-    tel = models.TextField()
